0b13588bc3d839db818c311cc97590787e0dcac2
dark-tulip <89765480+dark-tulip@users.noreply.github.com>
2022-08-14 14:57:20 +0600

    Create simple-nums-counter.py

---
+++ simple-nums-counter.py
@@ -0,0 +1,22 @@
+def is_simple(num : int):
+    sqrt_num = int(num ** 0.5) + 1
+    for i in range(2, sqrt_num ):
+        if (num % i == 0):
+            return 0
+    return 1
+
+
+def count_simple_nums(number : int):
+
+    if number == 1: 
+        return 1
+    if is_simple(number):
+        return number - 1
+
+    cnt = 0
+    for i in range(2, number):
+        cnt += (1 if number % i else 0)
+    return cnt
+
+
+print(count_simple_nums(4344))
